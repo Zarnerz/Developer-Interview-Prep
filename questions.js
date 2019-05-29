8216031e2d4198cd9f0862905d22f261cb7cce3d
Dale Holstein <holstein30@live.com>
2019-05-29 18:35:49 -0400

    first round of general ?'s added

--- questions.js
+++ questions.js
@@ -1,1 +1,84 @@
 // Data for Mongo seeding
+
+const data = [
+  {
+    questionType: "general",
+    question: "Tell me about yourself"
+  },
+  {
+    questionType: "general",
+    question: "What excites or interests you about coding/development?"
+  },
+  {
+    questionType: "general",
+    question: "What did you learn yesterday/this week?"
+  },
+  {
+    questionType: "general",
+    question:
+      "What is a recent technical challenge you experienced and how did you solve it?"
+  },
+  {
+    questionType: "general",
+    question: "What is your greatest strength?"
+  },
+  {
+    questionType: "general",
+    question: "What is your greatest weakness?"
+  },
+  {
+    questionType: "general",
+    question: "Tell me about a time you exhibited leadership."
+  },
+  {
+    questionType: "general",
+    question: "Tell me about a time you had to work as a team."
+  },
+  {
+    questionType: "general",
+    question:
+      "Tell me about a time you've had to work with difficult people/person"
+  },
+  {
+    questionType: "general",
+    question: "Tell me about a time you failed."
+  },
+  {
+    questionType: "general",
+    question: "Tell me about a time you had success."
+  },
+  {
+    questionType: "general",
+    question: "Where do you see yourself in the next 5/10 years?"
+  },
+  {
+    questionType: "general",
+    question: "Why hire you?"
+  },
+  {
+    questionType: "general",
+    question: "Give me a reason not to hire you."
+  },
+  {
+    questionType: "general",
+    question:
+      "What is a recent technical challenge you experienced and how did you solve it?"
+  },
+  {
+    questionType: "general",
+    question:
+      "If you could master one technology this year, what would it be and why?"
+  },
+  {
+    questionType: "general",
+    question: "Talk about your preferred development environment."
+  },
+  {
+    questionType: "general",
+    question: "What's a cool project that you've recently worked on?"
+  },
+  {
+    questionType: "general",
+    question: "Do you have any pet projects? What kind?"
+  }
+];
